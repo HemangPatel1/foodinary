804595ec8c0c6ede11db3789de1c0ce2fd4ded97
HemangPatel1 <hemang2@gmail.com>
2013-11-29 12:18:15 +0000

    created rails app and set up tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodinary::Application.config.secret_key_base = 'ad9fcef308a259263c012d0a2773128dacdff560c9aaa4989866a3acaaac61011aa766044212a5bc2059e714aa8f90bcb84b61a48ffb95d7d9f7f81ebcd84fd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foodinary::Application.config.session_store :cookie_store, key: '_foodinary_session'

46cd51fe6a0d85226ea156e612bbcbc0d59d5de9
HemangPatel1 <hemang2@gmail.com>
2013-11-30 10:24:06 +0000

    added restaurant id to reviews table and set up has many and belongs to relationships, tests pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130092445) do
+ActiveRecord::Schema.define(version: 20131130102247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20131130092445) do
     t.string   "review_body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "restaurant_id"
   end
 
 end
